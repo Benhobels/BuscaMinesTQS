cc26cf18c253aafdea6813542d2ef4c10c99806d
AdriaCuscullola <adria.cuscullola@e-campus.uab.cat>
2020-11-04 19:08:54 +0100

    Usuario selecciona la dificultad normal -- TDD

---
+++ src/main/java/BuscaMines/MockPlayer.java
@@ -0,0 +1,8 @@
+package BuscaMines;
+
+public class MockPlayer implements Player {
+	public int seleccionarDificultad() {
+		return 1; // dificultad normal
+	}
+
+}
--- src/main/java/BuscaMines/Player.java
+++ src/main/java/BuscaMines/Player.java
@@ -1,5 +1,5 @@
 package BuscaMines;
 
 public interface Player {
-
+	public abstract int seleccionarDificultad();
 }
