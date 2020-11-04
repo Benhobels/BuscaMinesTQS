5b4945c508c31d756ba71fc4b2a4364ed9b050d4
AdriaCuscullola <adria.cuscullola@e-campus.uab.cat>
2020-11-04 10:20:45 +0100

    Resultado OK de Path Coverage para la función expandir borde derecho

---
+++ src/main/java/BuscaMines/MockPathCoverage.java
@@ -0,0 +1,27 @@
+package BuscaMines;
+
+public class MockPathCoverage implements GeneradorRandom{
+	public Casella[][] generarMinas(int filas, int columnas, int minas) {
+        Casella[][] matriz = new Casella[filas][columnas];
+        
+        //Aquest bucle hauria de anar fora potser
+        for(int i = 0; i < filas; i++) {
+        	for(int j = 0; j < columnas; j++) {
+        		matriz[i][j] = new Casella();
+        	}
+        }
+        
+        matriz[0][0].setValor(9);
+        matriz[0][7].setValor(9);
+        matriz[7][0].setValor(9);
+        matriz[7][7].setValor(9);
+        matriz[0][2].setValor(9);
+        matriz[0][4].setValor(9);
+        matriz[0][5].setValor(9);
+        matriz[3][1].setValor(9);
+        matriz[5][1].setValor(9);
+        matriz[5][4].setValor(9);
+        
+        return matriz;
+    }
+}
