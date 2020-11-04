5c421c5516e2b06e553706dac2bc2b5eeb61a757
AdriaCuscullola <adria.cuscullola@e-campus.uab.cat>
2020-11-04 19:39:57 +0100

    Test usuario selecciona casilla -- TDD

--- src/test/java/BuscaMines/PlayerTest.java
+++ src/test/java/BuscaMines/PlayerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class PlayerTest {
 
+	// 1- test en el que el usuario selecciona una dificultad válida
 	@Test
 	public void testSelectDificultadNormal() {
 		Player p = new MockPlayer();
@@ -14,6 +15,24 @@ public class PlayerTest {
 		assertEquals(dificultad, 1);
 	}
 	
+	// 2- test en el que el usuario selecciona una dificultad no válida --try catch?---
+	
+	// 3- test en el que el usuario selecciona una casilla válida 
+	@Test
+	public void testSelectCasilla() {
+		Player p = new MockPlayer();
+		int[] posCasilla = new int[2];
+		posCasilla = p.seleccionarCasilla();
+		assertEquals(posCasilla[0], 2);
+		assertEquals(posCasilla[1], 6);
+		
+	}
+	
+	// 4- test en el que el usuario selecciona una casilla inválida -- fuera de rango --
+	
+	// 5- test en el que el usuario selecciona una casilla inválida -- ya abierta --
+	
+	// 6- test en el que el propio usuario entra un valor --cin--
 
 	
 
